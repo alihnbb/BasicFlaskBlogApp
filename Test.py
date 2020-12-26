16d29ac2f69d3e765df96cc4cb445069e0d847bf
alihnbb <alihnbb@gmail.com>
2020-12-26 15:41:22 +0300

    gereksizler silindi

--- Test.py
+++
@@ -1,9 +0,0 @@
-print("test")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-print("yeni yazı")
->>>>>>> parent of d267f49... gereksi dosyalar silindi
-=======
-print("yeni yazı")
->>>>>>> parent of d267f49... gereksi dosyalar silindi
